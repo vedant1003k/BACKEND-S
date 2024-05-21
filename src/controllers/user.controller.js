ec763738e0535d9a26487442ce28b26252d4794e
Vedant Kashyap <varun.567.kashyap@gmail.com>
2024-05-21 23:43:21 +0530

    add register controller and bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   //   res.status(200).json({
@@ -76,7 +77,19 @@ const registerUser = asyncHandler(async (req, res) => {
     username,
   });
 
-  
+  //check whether the user is authenticated or not
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "somthing went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registerd Successfully"));
+
   // res.send(respone);
 });
 
